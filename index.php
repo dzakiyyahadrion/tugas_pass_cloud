8ff361d8d7bf215c983e19683a75baa690e8336d
dzakiyyahadrion <55857729+dzakiyyahadrion@users.noreply.github.com>
2020-09-24 12:02:11 +0700

    Update index.php

--- index.php
+++ index.php
@@ -35,8 +35,6 @@
     border-color: #f1f1f1;
   }
   .carousel-inner img {
-    -webkit-filter: grayscale(90%);
-    filter: grayscale(90%); /* make all photos black and white */ 
     width: 100%; /* Set width to 100% */
     margin: auto;
   }
@@ -197,16 +195,16 @@
       <div class="item">
         <img src="uin-jakarta2.jpg" alt="Chicago" width="1200" height="700">
         <div class="carousel-caption">
-          <h3>Chicago</h3>
-          <p>Thank you, Chicago - A night we won't forget.</p>
+          <h3>UIN Syarif Hidayatullah Jakarta</h3>
+          <p>Awalnya merupakan Akademi Dinas Departemen Agama (ADIA) yang berdiri pada tahun 1957.</p>
         </div>      
       </div>
     
       <div class="item">
         <img src="uin-jakarta3.jpg" alt="Los Angeles" width="1200" height="700">
         <div class="carousel-caption">
-          <h3>LA</h3>
-          <p>Even though the traffic was a mess, we had the best time playing at Venice Beach!</p>
+          <h3>UIN Syarif Hidayatullah Jakarta</h3>
+          <p>Hingga pada tahun 2002 resmi menjadi UIN Syarif Hidayatullah Jakarta</p>
         </div>      
       </div>
     </div>
@@ -230,7 +228,7 @@
   <br>
   <div class="row">
     <div class="col-sm-4">
-      <p class="text-center"><strong>Name</strong></p><br>
+      <p class="text-center"><strong>Auditorium</strong></p><br>
       <a href="#demo" data-toggle="collapse">
         <img src="bandmember.jpg" class="img-circle person" alt="Random Name" width="255" height="255">
       </a>
@@ -241,7 +239,7 @@
       </div>
     </div>
     <div class="col-sm-4">
-      <p class="text-center"><strong>Name</strong></p><br>
+      <p class="text-center"><strong>Perpustakaan</strong></p><br>
       <a href="#demo2" data-toggle="collapse">
         <img src="bandmember.jpg" class="img-circle person" alt="Random Name" width="255" height="255">
       </a>
@@ -252,7 +250,7 @@
       </div>
     </div>
     <div class="col-sm-4">
-      <p class="text-center"><strong>Name</strong></p><br>
+      <p class="text-center"><strong>Kantin</strong></p><br>
       <a href="#demo3" data-toggle="collapse">
         <img src="bandmember.jpg" class="img-circle person" alt="Random Name" width="255" height="255">
       </a>
@@ -260,43 +258,6 @@
         <p>Bass player</p>
         <p>Loves math</p>
         <p>Member since 2005</p>
-      </div>
-    </div>
-  </div>
-</div>
-
-
-  <!-- Modal -->
-  <div class="modal fade" id="myModal" role="dialog">
-    <div class="modal-dialog">
-    
-      <!-- Modal content-->
-      <div class="modal-content">
-        <div class="modal-header">
-          <button type="button" class="close" data-dismiss="modal">×</button>
-          <h4><span class="glyphicon glyphicon-lock"></span> Tickets</h4>
-        </div>
-        <div class="modal-body">
-          <form role="form">
-            <div class="form-group">
-              <label for="psw"><span class="glyphicon glyphicon-shopping-cart"></span> Tickets, $23 per person</label>
-              <input type="number" class="form-control" id="psw" placeholder="How many?">
-            </div>
-            <div class="form-group">
-              <label for="usrname"><span class="glyphicon glyphicon-user"></span> Send To</label>
-              <input type="text" class="form-control" id="usrname" placeholder="Enter email">
-            </div>
-              <button type="submit" class="btn btn-block">Pay 
-                <span class="glyphicon glyphicon-ok"></span>
-              </button>
-          </form>
-        </div>
-        <div class="modal-footer">
-          <button type="submit" class="btn btn-danger btn-default pull-left" data-dismiss="modal">
-            <span class="glyphicon glyphicon-remove"></span> Cancel
-          </button>
-          <p>Need <a href="#">help?</a></p>
-        </div>
       </div>
     </div>
   </div>
@@ -311,8 +272,8 @@
     <div class="col-md-4">
       <p>Fan? Drop a note.</p>
       <p><span class="glyphicon glyphicon-map-marker"></span>Chicago, US</p>
-      <p><span class="glyphicon glyphicon-phone"></span>Phone: +00 1515151515</p>
-      <p><span class="glyphicon glyphicon-envelope"></span>Email: mail@mail.com</p>
+      <p><span class="glyphicon glyphicon-phone"></span>Phone: +6285156106964</p>
+      <p><span class="glyphicon glyphicon-envelope"></span>Email: annisadzakiyyah.hanifah18@mhs.uinjkt.ac.id</p>
     </div>
    
   </div>
